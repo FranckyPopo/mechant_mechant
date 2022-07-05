d174fd0727e88438cb9318ca80b5d90186aadf6e
FranckyPopo <afrifranck2003@gmail.com>
2022-07-05 12:56:11 +0000

    Ajout et ammélioration du models de l'application front

--- src/front/models.py
+++ src/front/models.py
@@ -1,11 +1,14 @@
 
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
+from colorfield.fields import ColorField
 
 class Categories(models.Model):
     name = models.fields.CharField(max_length=150)
+    image = models.ImageField()
+    description = models.CharField(max_length=150)
     active = models.BooleanField(default=True)
     
     updated = models.fields.DateTimeField(auto_now=True)
@@ -17,20 +20,43 @@ class Categories(models.Model):
 
 class Products(models.Model):
     name = models.CharField(max_length=150)
-    photo = models.ImageField()
     description = models.TextField()
-    original_price = models.PositiveIntegerField(validators=[MaxValueValidator(100)])
-    promotion_price = models.PositiveIntegerField(validators=[MaxValueValidator(100)])
-    promotion_percentage = models.PositiveIntegerField(validators=[MaxValueValidator(100)])
-    additional_information = models.TextField()
-    promotion = models.BooleanField(default=False)
-    active = models.BooleanField(default=False)
-    categories = models.ManyToManyField(Categories, related_name="product_categories")
+    photo = models.ImageField()
     
+    original_price = models.PositiveIntegerField()
+    promotion_price = models.PositiveIntegerField()
+    promotion_percentage = models.PositiveIntegerField(
+        validators=[MaxValueValidator(100), 
+        MinValueValidator(1)], 
+        default=1
+    )
+    additional_information = models.TextField()
+    categories = models.ManyToManyField(Categories, related_name="product_categories")
     color = models.CharField(max_length=50, blank=True)
     size = models.CharField(max_length=50, blank=True)
     longeur = models.CharField(max_length=50, blank=True)
     largeur = models.CharField(max_length=50, blank=True)
+    stock = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
+    
+    is_promotion = models.BooleanField(default=False)
+    is_sold = models.BooleanField(default=False)
+    active = models.BooleanField(default=False)
+    
+    updated = models.fields.DateTimeField(auto_now=True)
+    created = models.fields.DateTimeField(auto_now_add=True)
+    deleted = models.fields.BooleanField(default=False)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class ProductColor(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    code_hex = ColorField(default="#FF0000")
+    product = models.ForeignKey(
+        Products, 
+        related_name="protruct_color", 
+        on_delete=models.CASCADE
+    )
     
     updated = models.fields.DateTimeField(auto_now=True)
     created = models.fields.DateTimeField(auto_now_add=True)
@@ -40,8 +66,16 @@ class Products(models.Model):
         return self.name
 
 class Comments(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="user_comment")
-    product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name="comment_product")
+    user = models.ForeignKey(
+        get_user_model(), 
+        on_delete=models.CASCADE, 
+        related_name="user_comment"
+    )
+    product = models.ForeignKey(
+        Products, 
+        on_delete=models.CASCADE, 
+        related_name="comment_product"
+    )
     message = models.TextField()
     
     updated = models.fields.DateTimeField(auto_now=True)
@@ -60,10 +94,14 @@ class ImageProduct(models.Model):
     deleted = models.fields.BooleanField(default=False)
     
     def __str__(self) -> str:
-        return str(self.product.name)
+        return self.product.name
 
 class Promotion(models.Model):
-    product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name="promotion_product")
+    product = models.ForeignKey(
+        Products, 
+        on_delete=models.CASCADE, 
+        related_name="promotion_product"
+    )
     active = models.BooleanField(default=True)
     
     updated = models.fields.DateTimeField(auto_now=True)
@@ -71,18 +109,22 @@ class Promotion(models.Model):
     deleted = models.fields.BooleanField(default=False)
     
     def __str__(self) -> str:
-        return self.title    
+        return self.product.name    
     
 class DealOfTheWeenk(models.Model):
     start_of_deal = models.DateTimeField()
-    product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name="DealOfTheWeenk_product")
+    product = models.ForeignKey(
+        Products, 
+        on_delete=models.CASCADE, 
+        related_name="DealOfTheWeenk_product"
+    )
     
     updated = models.fields.DateTimeField(auto_now=True)
     created = models.fields.DateTimeField(auto_now_add=True)
     deleted = models.fields.BooleanField(default=False)
     
     def __str__(self) -> str:
-        return self.product
+        return self.product.name
 
 class BestSellers(models.Model):
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
