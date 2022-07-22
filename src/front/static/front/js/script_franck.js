0f7f93c99c0a4819fcdfc8cb971d04f889c263d1
FranckyPoop <afrifranck2003@gmail.com>
2022-07-22 03:21:02 +0000

    Le temps de la notification est passé de 3 a 2 séconde

--- src/front/static/front/js/script_franck.js
+++ src/front/static/front/js/script_franck.js
@@ -10,7 +10,7 @@ function toast(text, e) {
         toast: true,
         position: 'top-end',
         showConfirmButton: false,
-        timer: 3000
+        timer: 2000
       })
       
       Toast.fire({
