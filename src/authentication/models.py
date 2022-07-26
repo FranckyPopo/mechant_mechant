691d0d8d0f70ce4c9a3061679341709a1d03eee1
FranckyPoop <afrifranck2003@gmail.com>
2022-07-26 15:12:38 +0000

    Ajout du champ addresse au model User

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -2,8 +2,16 @@ from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 from django.db import models
 
+from front.models import DeliveryAddress
+
 class User(AbstractUser):
     telephone_number = PhoneNumberField(unique=True, region="CI")
+    delivery_address = models.ManyToManyField(
+        DeliveryAddress,
+        blank=True,
+        null=True,
+        related_name="user_delivery_address"
+    )
     
     def __str__(self) -> str:
         return self.username
