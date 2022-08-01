2b9cf41a34ae1380b2ea70c59e64d8b5e26ae883
FranckyPoop <afrifranck2003@gmail.com>
2022-08-01 21:19:22 +0000

    Ajout de nouveau paramétre a au champ slug

--- src/front/models.py
+++ src/front/models.py
@@ -24,7 +24,7 @@ class Products(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     photo = models.ImageField()
-    slug = models.SlugField()
+    slug = models.SlugField(null=True, blank=True)
     original_price = models.PositiveIntegerField()
     promotion_reduction = models.PositiveIntegerField(
         validators=[MinValueValidator(0)],
